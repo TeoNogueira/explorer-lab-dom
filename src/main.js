96940b550ed547b0c6fa3f271c9ad067bdd79888
Teo Nogueira <teonogueira17@gmail.com>
2022-10-20 21:37:57 -0300

    FINISHED-PROJECT-EXPLORER-LAB-01-DOM

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ const ccBgColor01 = document.querySelector('.cc-bg svg > g g:nth-child(1) path')
 const ccBgColor02 = document.querySelector('.cc-bg svg > g g:nth-child(2) path');
 const ccLogo = document.querySelector('.cc-logo span:nth-child(2) img');
 
+
+
 function setCardType(type) {
 
 const colors = {
@@ -21,10 +23,9 @@ ccLogo.setAttribute('src', `cc-${type}.svg`)
 
 }
 
-setCardType("visa")
-
-// globalThis.setCardType = setCardType é o mesmo que usar a WINDOW
-
+// setCardType()
+ // globalThis, deixa a função global
+ globalThis.setCardType = setCardType
 
 const securityCode = document.querySelector('#security-code')
 const securityCodePattern = {
@@ -67,17 +68,17 @@ const cardNumberPattern = {
   mask: [
     {
       mask: "0000 0000 0000 0000",
-      regex: /^4\d{0, 15}/,
-      cardType: 'visa'
+      regex: /^4\d{0,15}/,
+      cardtype: 'visa'
     },
     {
       mask: "0000 0000 0000 0000",
       regex: /^(5[1-5]\d{0,2}|22[2-9]\d{0,1}|2[3-7]\d{0,2})\d{0,12}/,
-      cardType: 'mastercard'
+      cardtype: 'mastercard'
     },
     {
       mask: "0000 0000 0000 0000",
-      cardType: 'default',
+      cardtype: 'default',
     },
 
   ],
@@ -96,4 +97,76 @@ const cardNumberPattern = {
 }
 
 
-const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
+const cardNumberMasked = IMask(cardNumber, cardNumberPattern);
+
+// FORM 
+
+const addButton = document.querySelector('#add-card');
+
+const form = document.querySelector('form')
+
+addButton.addEventListener('click', (e) => {
+
+  alert('Clicado com sucesso!')
+
+})
+
+form.addEventListener('submit', (e) => {
+
+  e.preventDefault()
+});
+
+const cardHolder = document.querySelector('#card-holder')
+
+cardHolder.addEventListener('input', function() {
+  const ccHolder = document.querySelector('.cc-holder .value')
+  ccHolder.innerText = cardHolder.value.length === 0 ? "FULANO DA SILVA" : cardHolder.value
+
+})
+
+// accept evento do IMask 
+securityCodeMasked.on('accept', function() {
+updateSecurityCode(securityCodeMasked.value)
+
+})
+
+
+function updateSecurityCode(code) {
+
+  const ccSecurity = document.querySelector('.cc-security .value');
+  ccSecurity.innerText = code.length === 0 ? '123' : code
+
+}
+
+cardNumberMasked.on('accept', () => {
+
+  // Acessando o tipo da máscara do cartão e invocando função de cores e logos  de acordo com a máscara do cartão
+  const cardType = cardNumberMasked.masked.currentMask.cardtype
+  setCardType(cardType)
+  //----------------------------------------------------------
+  updateCardNumber(cardNumberMasked.value);
+})
+
+
+function updateCardNumber(number) {
+
+  const ccNumber = document.querySelector('.cc-number');
+  ccNumber.innerText = number.length === 0 ? '1234 5678 9012 3456' : number
+
+}
+
+expCodeMasked.on('accept', () => {
+
+
+updateExpirationDate(expCodeMasked.value)
+
+})
+
+function updateExpirationDate(date) {
+
+  const expDate = document.querySelector('.cc-extra .value');
+
+  expDate.innerText = date.length === 0 ? `09/${date10}` : date 
+
+
+}
